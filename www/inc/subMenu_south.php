adf48c7f958d5115ce9c3afa7fb2528694a5b4d1
devilkij77 <devilkij77@gmail.com>
2018-10-14 23:19:23 +0900

    left 하단 구입문의 부분 추가

--- www/inc/subMenu_south.php
+++ www/inc/subMenu_south.php
@@ -3,7 +3,7 @@
 		$menu_titleMenu_southText1_1 = "온라인 문의";
 		$menu_titleMenu_southText1_2 = "";
 		$menu_titleMenu_southText3 = "온라인상담 바로가기";
-		
+
 
 
 	}else{
@@ -18,26 +18,35 @@
   <p class="text2">powercoolair@naver.com</p>
   <a href="../support/estimate.php" class="onlineBtn"><?=$menu_titleMenu_southText3?></a>
 </div><!--.online-->
-<div class="searchWrap">
+<div class="counseling_info">
+	<p class="text1">구입문의<br /></p>
+	<p class="text2">055-783-1009</p>
+	<!--
   <p class="searchN">네이버</p>
   <input type="text" id="searchN" name="searchN" placeholder="파워쿨">
   <a href="https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&oquery=%ED%8C%8C%EC%9B%8C%EC%BF%A8&ie=utf8&query=powercoolair" class="inputBtn btnN"  target="_blank">검색</a>
   <p class="searchD">다음</p>
   <input type="text" id="searchD" name="searchD" placeholder="파워쿨">
   <a href="#" class="inputBtn btnD"  target="_blank">검색</a>
-</div><!--.searchWrap-->
+	-->
+</div>
+<!--.searchWrap-->
 <?php }else{?>
 <div class="online">
   <p class="text1" style="text-align:right; line-height:20px; padding-top:10px; padding-right:10px; background-position:10px -10px;"><?=$menu_titleMenu_southText1_1?><br /><?=$menu_titleMenu_southText1_2?></p>
   <p class="text2" style="margin-top:-10px;">powercoolair@naver.com</p>
   <a href="../support/estimate.php" class="onlineBtn" style="padding:5px 10px;"><?=$menu_titleMenu_southText3?></a>
 </div><!--.online-->
-<div class="searchWrap">
+<div class="counseling_info">
+	<p class="text1">구입문의<br /></p>
+	<p class="text2">055-783-1009</p>
+	<!--
   <p class="searchN">네이버</p>
   <input type="text" id="searchN" name="searchN" placeholder="파워쿨">
   <a href="https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&oquery=%ED%8C%8C%EC%9B%8C%EC%BF%A8&ie=utf8&query=powercoolair" class="inputBtn btnN"  target="_blank">검색</a>
   <p class="searchD">다음</p>
   <input type="text" id="searchD" name="searchD" placeholder="파워쿨">
   <a href="#" class="inputBtn btnD"  target="_blank">검색</a>
+	-->
 </div><!--.searchWrap-->
 <?php }?>
