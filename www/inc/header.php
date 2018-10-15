438b6d435d96d1a4ad334734f2bd4b23ac7aceb3
In joon. Gong <ijgong@nglp.kr>
2018-10-15 17:42:35 +0900

    cache reload를 위한 javascript method 추가

--- www/inc/header.php
+++ www/inc/header.php
@@ -57,6 +57,27 @@ if($lang=="kr"){
 ?>
 
 <script type="text/javascript">
+(function(){
+
+    // Match this timestamp with the release of your code
+    var lastVersioning = Date.UTC(2014, 11, 20, 2, 15, 10);
+
+    var lastCacheDateTime = localStorage.getItem('lastCacheDatetime');
+
+    if(lastCacheDateTime){
+        if(lastVersioning > lastCacheDateTime){
+            var reload = true;
+        }
+    }
+
+    localStorage.setItem('lastCacheDatetime', Date.now());
+
+    if(reload){
+        location.reload(true);
+    }
+
+})();
+
 function logout()
 {
 	document.formLogin.submit();
