7bd9cf77f1b577ab02edb403e39d555808487bf6
devilkij77 <devilkij77@gmail.com>
2018-10-15 22:21:42 +0900

    모델 및 규격화면 이미지간 margin 수정 footer copyright 문구 수정

--- www/inc/footer.php
+++ www/inc/footer.php
@@ -3,13 +3,13 @@
   <div id="footerin">
     <?php if ($lang == "kr") {?>
       <address class="company"><span>(주)파워쿨</span>사업자등록번호: 131-81-81946&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;대표이사: 윤종찬&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;경상남도 양산시 산막공단남11길 35&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TEL: 055-783-1121&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FAX: 055-783-1010 </address>
-      <p class="copy">copyright (C) 2016 by (주)파워쿨 All Rights Reserved.</p>
+      <p class="copy">ⓒ 2018 Powercool Co, Ltd., All Rights Reserved.</p>
 
     <?php } else {?>
       <address class="company" style="font-size:11px;"><span style="font-size:12px;">POWERCOOL Co., Ltd</span>Company Registration: 131-81-81946&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CEO: Yoon, Jongchan<br />
       35 Samakgongdannam 11-gil, Yangsan-si, Gyeongsangnam-do, Korea&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TEL: 055-783-1121&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FAX: 055-783-1010
       </address>
-      <p class="copy" style="font-size:11px;">copyright (C) 2016 by POWERCOOL Co,. LTD. All Rights Reserved</p>
+      <p class="copy" style="font-size:11px;">ⓒ 2018 Powercool, All Rights Reserved</p>
     <?php }?>
   </div><!--#footerin-->
 </div><!--#footer-->
