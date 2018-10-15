00ac9a579aef71980aeff622262788f6dd74a310
In joon. Gong <ijgong@nglp.kr>
2018-10-15 16:43:07 +0900

    하단 footer 수정

--- www/inc/footer_eng.php
+++ www/inc/footer_eng.php
@@ -1,6 +1,9 @@
 <div id="footer">
   <div id="footerin">
-    <address class="company"><span>POWERCOOL Co., Ltd</span>Company Registration Number  : 131-81-81946   CEO : Yun jongchan │ 35 Samakgongdannam 11-gil, Yangsan-si, Gyeongsangnam-do, Korea. │ TEL:+82-55-783-1121 │ FAX: +82-55-055-783-1010 </address>
+    <address class="company"><span>POWERCOOL Co., Ltd</span>
+      <pre>Company Registration: 131-81-81946     CEO: Yoon, Jongchan
+35 Samakgongdannam 11-gil, Yangsan-si, Gyeongsangnam-do, Korea     TEL: 055-783-1121     FAX: 055-783-1010
+      </pre></address>
     <p class="copy">copyright (C) 2016 by POWERCOOL Co,. LTD. All Rights Reserved</p>
   </div><!--#footerin-->
 </div><!--#footer-->
