b0a43a8f077a5670dd584309b7fadbf1071cc89d
In joon. Gong <ijgong@nglp.kr>
2018-10-16 18:17:06 +0900

    메인화면 하단 문구변경

--- www/main/main.php
+++ www/main/main.php
@@ -3,34 +3,34 @@
 
 if($lang=="kr"){
 	$menu_titleMenu_contentTitle1 = "온라인 문의";
-	$menu_titleMenu_contentTitle2 = "뉴스 및 공지";
+	$menu_titleMenu_contentTitle2 = "NEWS &amp; NOTICE";
 	$menu_titleMenu_contentTitle3 = "POWER <font color='blue'>COOL</font> &amp; <font color='red'>HEAT</font>";
-	$menu_titleMenu_contentText2_1_1 = "POWER COOL & HOT에 대해서 궁금한 점이 있으시거나 묻고 싶은 점이 있으시면 언제든 문의하십시오.";
-	$menu_titleMenu_contentText2_1_2 = "최대한 신속하고 빠른 답변으로 고객의 궁금증을 해소해 드리겠습니다.";
-	$menu_titleMenu_contentText2_2_1 = "POWER COOL & HOT의 뉴스와 새로운 소식을";
-	$menu_titleMenu_contentText2_2_2 = "알려드리는 곳입니다. 전시회 참가, 각종 행사나 이벤트 등 POWER COOL & HOT의 다양한 소식들을 만나보세요";
-	$menu_titleMenu_contentText2_3_1 = "POWER COOL & HOT 제품에 대해서 궁금하신가요?";
-	$menu_titleMenu_contentText2_3_2 = "제품의 특징 사양 그리고 POWER COOL & HOT의";
-	$menu_titleMenu_contentText2_3_3 = "제품 테스트 등 제품의 대한 궁금한 사항들이 소개되어 있습니다.";
-	$menu_titleMenu_conBtn1 = "온라인상담 바로가기";
-	$menu_titleMenu_conBtn2 = "NEWS 바로가기";
+	$menu_titleMenu_contentText2_1_1 = "POWER COOL&HEAT에 대해 문의사항이 있으실 경우 언제든 이메일 혹은 게시판으로 문의해주십시오.";
+	$menu_titleMenu_contentText2_1_2 = ""; //"최대한 신속하고 빠른 답변으로 고객의 궁금증을 해소해 드리겠습니다.";
+	$menu_titleMenu_contentText2_2_1 = "POWERCOOL&HEAT에 대해 각종 전시회 참가, 뉴스, 이벤트 등 새로운 소식을 알려드립니다.";
+	$menu_titleMenu_contentText2_2_2 = ""; //"알려드리는 곳입니다. 전시회 참가, 각종 행사나 이벤트 등 POWER COOL & HOT의 다양한 소식들을 만나보세요";
+	$menu_titleMenu_contentText2_3_1 = "POWERCOOL&HEAT 제품에 대해 궁금하신가요? ";
+	$menu_titleMenu_contentText2_3_2 = "제품의 특징, 모델, 규격 등 다양한 정보가 소개되어 있습니다.";
+	$menu_titleMenu_contentText2_3_3 = ""; //"제품 테스트 등 제품의 대한 궁금한 사항들이 소개되어 있습니다.";
+	$menu_titleMenu_conBtn1 = "온라인 문의 바로가기";
+	$menu_titleMenu_conBtn2 = "NEWS &amp; NOTICE 바로가기";
 	$menu_titleMenu_conBtn3 = "제품소개 바로가기";
 
 
 }else{
-	$menu_titleMenu_contentTitle1 = "Online Counseling";
-	$menu_titleMenu_contentTitle2 = "News &amp; Notice";
-	$menu_titleMenu_contentTitle3 = "POWER COOL &amp; HEAT";
-	$menu_titleMenu_contentText2_1_1 = "Please contact us any time if you have any curiosities about POWER COOL & HOT or if you have any questions to ask. We will strive to resolve the curiosities of our customers by the rapid answers as soon as possible.";
+	$menu_titleMenu_contentTitle1 = "CUSTOMER SUPPORT";
+	$menu_titleMenu_contentTitle2 = "NEWS &amp; NOTICE";
+	$menu_titleMenu_contentTitle3 = "POWER <font color='blue'>COOL</font> &amp; <font color='red'>HEAT</font>";
+	$menu_titleMenu_contentText2_1_1 = "If you have any inquiries about POWERCOOL&HEAT, please contact us via e-mail or online Q&A board any time. We are happy to listen to you and oblige.";
 	$menu_titleMenu_contentText2_1_2 = "";
-	$menu_titleMenu_contentText2_2_1 = "We provide the news and other updates of POWER COOL & HOT here. Please come in contact with a diversity of news at POWER COOL & HOT such as the participation at exhibitions, hosting of events of different types and others.";
+	$menu_titleMenu_contentText2_2_1 = "Updates on POWERCOOL&HEAT, such as exhibition, news, and events.";
 	$menu_titleMenu_contentText2_2_2 = "";
-	$menu_titleMenu_contentText2_3_1 = "Do you have any curiosities about the products of POWER COOL & HOT? The product details to quench the curiosities are introduced here including the major characteristics and specifications of products, the test results of the products of POWER COOL & HOT and others.";
+	$menu_titleMenu_contentText2_3_1 = "Learn about POWERCOOL&HEAT. Here, we introduce POWERCOOL&HEAT including the features, models, and specifications.";
 	$menu_titleMenu_contentText2_3_2 = "";
 	$menu_titleMenu_contentText2_3_3 = "";
-	$menu_titleMenu_conBtn1 = "Go to Online Counseling";
-	$menu_titleMenu_conBtn2 = "Go to News";
-	$menu_titleMenu_conBtn3 = "Go to Introduction of Products";
+	$menu_titleMenu_conBtn1 = "ONLINE Q &amp; A";
+	$menu_titleMenu_conBtn2 = "NEWS &amp; NOTICE";
+	$menu_titleMenu_conBtn3 = "ABOUT POWERCOOL&amp;HEAT";
 }
 
 ?>
